c917f29ab18d2426c7d7032eedcfb6e0b4059520
Dzmitry <koirn.dev@gmail.com>
2020-12-18 14:01:14 +0300

    Added Android skeleton application

---
+++ android-app/app/src/main/java/by/dzkoirn/cristmastree/ChristmasTreeWallpapperDemoActivity.kt
@@ -0,0 +1,11 @@
+package by.dzkoirn.cristmastree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChristmasTreeWallpapperDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
---
+++ android-app/app/src/main/java/by/dzkoirn/cristmastree/ChristmasTreeWallpapperService.kt
@@ -0,0 +1,13 @@
+package by.dzkoirn.cristmastree
+
+import android.service.wallpaper.WallpaperService
+
+class ChristmasTreeWallpapperService : WallpaperService() {
+    override fun onCreateEngine(): Engine {
+        TODO("Not yet implemented")
+    }
+
+    private inner class WallpaperEngine : WallpaperService.Engine() {
+
+    }
+}
